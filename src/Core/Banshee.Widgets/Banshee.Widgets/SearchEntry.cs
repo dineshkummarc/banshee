960d47bb546e78b52d43b8bb2ccdcf1d08f1b3b1
Mike Urbanski <michael.c.urbanski@gmail.com>
2010-03-29 22:56:35 -0500

    [SearchEntry] Added 'Cleared' event

--- src/Core/Banshee.Widgets/Banshee.Widgets/SearchEntry.cs
+++ src/Core/Banshee.Widgets/Banshee.Widgets/SearchEntry.cs
@@ -50,6 +50,8 @@ namespace Banshee.Widgets
         private event EventHandler filter_changed;
         private event EventHandler entry_changed;
 
+        public event EventHandler<EventArgs> Cleared;
+
         public event EventHandler Changed {
             add { entry_changed += value; }
             remove { entry_changed -= value; }
@@ -238,6 +240,12 @@ namespace Banshee.Widgets
         {
             active_filter_id = 0;
             entry.Text = String.Empty;
+
+            EventHandler<EventArgs> handler = Cleared;
+
+            if (handler != null) {
+                handler (this, EventArgs.Empty);
+            }
         }
 
         protected override bool OnKeyPressEvent (Gdk.EventKey evnt)
