7281b3eaf6eebc7cf5e677e78b5a6ae2dc679d11
Gabriel Burt <gabriel.burt@gmail.com>
2010-08-12 18:03:54 -0700

    [TrackActions] Prevent crash in search-for-same
    Specifically, when artist/album are blank/null.  Fixes bgo#626786.

--- src/Core/Banshee.ThickClient/Banshee.Gui/TrackActions.cs
+++ src/Core/Banshee.ThickClient/Banshee.Gui/TrackActions.cs
@@ -474,7 +474,9 @@ namespace Banshee.Gui
         {
             if (current_source != null) {
                 foreach (TrackInfo track in current_source.TrackModel.SelectedItems) {
-                    ActiveSource.FilterQuery = BansheeQuery.ArtistField.ToTermString (":", track.ArtistName);
+                    if (!String.IsNullOrEmpty (track.ArtistName)) {
+                        ActiveSource.FilterQuery = BansheeQuery.ArtistField.ToTermString (":", track.ArtistName);
+                    }
                     break;
                 }
             }
@@ -484,7 +486,9 @@ namespace Banshee.Gui
         {
             if (current_source != null) {
                 foreach (TrackInfo track in current_source.TrackModel.SelectedItems) {
-                    ActiveSource.FilterQuery = BansheeQuery.AlbumField.ToTermString (":", track.AlbumTitle);
+                    if (!String.IsNullOrEmpty (track.AlbumTitle)) {
+                        ActiveSource.FilterQuery = BansheeQuery.AlbumField.ToTermString (":", track.AlbumTitle);
+                    }
                     break;
                 }
             }
